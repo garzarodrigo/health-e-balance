df3f93b15d3f55f1f9af028e69887bd3f989ccc7
garzarodrigo <rgarza2016@kellogg.northwestern.edu>
2015-06-08 23:56:23 -0500

    progress

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,11 +4,34 @@ class PagesController < ApplicationController
 
   def index
 
-    @meals = current_user.meals
-    @activities = current_user.activities
+    @meals = current_user.meals.where(:date => Date.today)
+    @activities = current_user.activities.where(:date => Date.today)
+
+    # BMR=66.47+ (13.75 x W) + (5.0 x H) - (6.75 x A)
+
+    # calfactor = nil
+
+    # if current_user.lifestyle == "sedentary"
+    #  calfactor = 0.75
+    #  if current_user.lifestyle == "active"
+    #   calfactor = 1.25
+    #  else
+
+    # end
+    #  calfactor = 1.5
+    # end
+
+    now = Time.now.utc.to_date
+    age = now.year - current_user.birthday.year - (current_user.birthday.to_date.change(:year => now.year) > now ? 1 : 0)
+
+
+    @benchmarkcals = (66.47 + (13.75*current_user.weight/2.2) + (5*((current_user.heightft*12 + current_user.heightin)*2.54)) - (6.75 * age))
 
 
   end
+
+
+
 
 
 
--- app/models/meal.rb
+++ app/models/meal.rb
@@ -3,5 +3,7 @@ class Meal < ActiveRecord::Base
   belongs_to :profile , :class_name => "User", :foreign_key => "profile_id"
 
 
-
+  def total_calories
+    return self.calportion * self.numportions
+  end
 end
